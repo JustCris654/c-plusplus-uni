bc770853fd6ee5f28ed919adc956d8a00c4a3877
JustCris <cristian.scapin654@icloud.com>
2021-11-09 09:39:04 +0100

    args_2 es print file content

---
+++ args_2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    assert(argc == 2 && "Usage: ./main [file_path]");
+
+    string file_path = argv[1];
+    fstream file;
+
+    file.open(file_path, ios::in);
+    if(file.is_open()){
+        string line="";
+        while(getline(file, line)){
+            cout << line << "\n";
+        }
+    }
+
+    return 0;
+}
